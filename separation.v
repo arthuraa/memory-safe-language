7d2492d401ee628381c0f7222fb79b0e572f1f9e
Arthur Azevedo de Amorim <arthur.aa@gmail.com>
2015-06-04 18:29:42 -0300

    Begin formalization.

--- separation.v
+++ separation.v
@@ -6,6 +6,10 @@ Require Import MathComp.ssrnum MathComp.ssrint MathComp.ssralg.
 Require Import CoqUtils.ord CoqUtils.fset CoqUtils.partmap CoqUtils.nominal.
 Require Import CoqUtils.string.
 
+Set Implicit Arguments.
+Unset Strict Implicit.
+Unset Printing Implicit Defensive.
+
 Section Def.
 
 Local Open Scope ring_scope.
@@ -26,50 +30,61 @@ Canonical int_ordType :=
 Definition int_nominalMixin := TrivialNominalMixin int.
 Canonical int_nominalType := Eval hnf in NominalType int int_nominalMixin.
 
+Definition string_nominalMixin := TrivialNominalMixin string.
+Canonical string_nominalType :=
+  Eval hnf in NominalType string string_nominalMixin.
+
 Inductive binop : Type :=
 | Add | Mul | Sub | Eq | Leq | And | Or.
 
-Inductive loc : Type :=
+Inductive expr :=
 | Var of string
-| Deref of expr
-
-with expr : Type :=
 | Num of int
-| Loc of loc
 | Binop of binop & expr & expr
-| Alloc of expr
+| ENil.
+
+Inductive com : Type :=
+| Assn of string & expr
+| Load of string & expr
+| Store of expr & expr
+| Alloc of string & expr
+| Free of expr
 | Skip
-| Seq of expr & expr
-| Let of string & expr & expr
-| Ass of loc & expr
-| If of expr & expr & expr
-| While of expr & expr.
+| Seq of com & com
+| If of expr & com & com
+| While of expr & com.
 
 Definition ptr : Type := (name * int)%type.
 
 Inductive value :=
 | VBool of bool
 | VNum  of int
-| VPtr  of name * int.
+| VPtr  of ptr
+| VNil.
+
+Definition is_vnat v :=
+  if v is VNum (Posz _) then true else false.
 
 Definition sum_of_value v :=
   match v with
   | VBool b => inl b
   | VNum n => inr (inl n)
-  | VPtr p => inr (inr p)
+  | VPtr p => inr (inr (inl p))
+  | VNil => inr (inr (inr tt))
   end.
 
 Definition value_of_sum v :=
   match v with
   | inl b => VBool b
   | inr (inl n) => VNum n
-  | inr (inr p) => VPtr p
+  | inr (inr (inl p)) => VPtr p
+  | inr (inr (inr tt)) => VNil
   end.
 
 Lemma sum_of_valueK : cancel sum_of_value value_of_sum.
 Proof. by case. Qed.
 Lemma value_of_sumK : cancel value_of_sum sum_of_value.
-Proof. by do 2!case. Qed.
+Proof. by do ![case=>//]. Qed.
 Definition value_eqMixin := CanEqMixin sum_of_valueK.
 Canonical value_eqType := Eval hnf in EqType value value_eqMixin.
 Definition value_partOrdMixin := CanPartOrdMixin sum_of_valueK.
@@ -81,7 +96,7 @@ Definition value_nominalMixin := BijNominalMixin sum_of_valueK value_of_sumK.
 Canonical value_nominalType :=
   Eval hnf in NominalType value value_nominalMixin.
 
-Definition locals := {partmap string -> name}.
+Definition locals := {partmap string -> value}.
 Definition heap := {partmap name * int -> value}.
 
 Implicit Types (ls : locals) (h : heap).
@@ -95,81 +110,133 @@ Definition free h b :=
 
 Definition eval_binop b v1 v2 :=
   match b, v1, v2 with
-  | Add, VNum n1, VNum n2 => Some (VNum (n1 + n2))
+  | Add, VNum n1, VNum n2 => VNum (n1 + n2)
   | Add, VPtr p, VNum n
-  | Add, VNum n, VPtr p => Some (VPtr (p.1, p.2 + n))
-  | Add, _, _ => None
-  | Sub, VNum n1, VNum n2 => Some (VNum (n1 - n2))
-  | Sub, VPtr p, VNum n => Some (VPtr (p.1, p.2 - n))
-  | Sub, _, _ => None
-  | Mul, VNum n1, VNum n2 => Some (VNum (n1 * n2))
-  | Mul, _, _ => None
-  | Eq, _, _ => Some (VBool (v1 == v2))
-  | Leq, VNum n1, VNum n2 => Some (VBool (n1 <= n2))
-  | Leq, _, _ => None
-  | And, VBool b1, VBool b2 => Some (VBool (b1 && b2))
-  | And, _, _ => None
-  | Or, VBool b1, VBool b2 => Some (VBool (b1 || b2))
-  | Or, _, _ => None
+  | Add, VNum n, VPtr p => VPtr (p.1, p.2 + n)
+  | Add, _, _ => VNil
+  | Sub, VNum n1, VNum n2 => VNum (n1 - n2)
+  | Sub, VPtr p, VNum n => VPtr (p.1, p.2 - n)
+  | Sub, _, _ => VNil
+  | Mul, VNum n1, VNum n2 => VNum (n1 * n2)
+  | Mul, _, _ => VNil
+  | Eq, _, _ => VBool (v1 == v2)
+  | Leq, VNum n1, VNum n2 => VBool (n1 <= n2)
+  | Leq, _, _ => VNil
+  | And, VBool b1, VBool b2 => VBool (b1 && b2)
+  | And, _, _ => VNil
+  | Or, VBool b1, VBool b2 => VBool (b1 || b2)
+  | Or, _, _ => VNil
   end.
 
-Inductive eval_loc ls h : loc -> ptr -> heap -> Prop :=
-| EVar v b of ls v = Some b : eval_loc ls h (Var v) (b, 0) h
+Fixpoint eval_expr ls e :=
+  match e with
+  | Var x => odflt VNil (ls x)
+  | Num n => VNum n
+  | Binop b e1 e2 => eval_binop b (eval_expr ls e1) (eval_expr ls e2)
+  | ENil => VNil
+  end.
 
-| EDeref e p h'
-  of eval_expr ls h e (VPtr p) h'
-  :  eval_loc  ls h (Deref e) p h'
+Inductive result :=
+| Done of locals & heap
+| Error
+| NotYet.
 
-with eval_expr ls h : expr -> value -> heap -> Prop :=
-| ENum n : eval_expr ls h (Num n) (VNum n) h
+Definition sum_of_result r :=
+  match r with
+  | Done ls h => inl (ls, h)
+  | Error => inr true
+  | NotYet => inr false
+  end.
 
-| ELoc l p v h'
-  of eval_loc ls h l p h'
-  &  h' p = Some v
-  :  eval_expr ls h (Loc l) v h'
+Definition result_of_sum r :=
+  match r with
+  | inl (ls, h) => Done ls h
+  | inr true => Error
+  | inr false => NotYet
+  end.
 
-| EBinop b e1 v1 h' e2 v2 h'' v
-  of eval_expr ls h e1 v1 h'
-  &  eval_expr ls h' e2 v2 h''
-  &  eval_binop b v1 v2 = Some v
-  :  eval_expr ls h (Binop b e1 e2) v h''
+Lemma sum_of_resultK : cancel sum_of_result result_of_sum.
+Proof. by case. Qed.
+Lemma result_of_sumK : cancel result_of_sum sum_of_result.
+Proof. by do ![case=>//]. Qed.
 
-| EAlloc e n h'
-  of eval_expr ls h e (VNum (Posz n)) h'
-  :  eval_expr ls h (Alloc e) (VPtr ((alloc h' n).1, 0)) (alloc h' n).2
+Definition result_eqMixin := CanEqMixin sum_of_resultK.
+Canonical result_eqType := Eval hnf in EqType result result_eqMixin.
+Definition result_partOrdMixin := CanPartOrdMixin sum_of_resultK.
+Canonical result_partOrdType :=
+  Eval hnf in PartOrdType result result_partOrdMixin.
+Definition result_ordMixin := CanOrdMixin sum_of_resultK.
+Canonical result_ordType := Eval hnf in OrdType result result_ordMixin.
+Definition result_nominalMixin := BijNominalMixin sum_of_resultK result_of_sumK.
+Canonical result_nominalType :=
+  Eval hnf in NominalType result result_nominalMixin.
 
-| ESkip : eval_expr ls h Skip (VNum 0) h
+Fixpoint eval_com ls h c k :=
+  if k is S k' then
+    match c with
+    | Assn x e =>
+      Done (setm ls x (eval_expr ls e)) h
 
-| ESeq e1 v1 h' e2 v2 h''
-  of eval_expr ls h e1 v1 h'
-  &  eval_expr ls h e2 v2 h''
-  :  eval_expr ls h (Seq e1 e2) (VNum 0) h''
+    | Load x e =>
+      if eval_expr ls e is VPtr p then
+        if h p is Some v then Done (setm ls x v) h
+        else Error
+      else Error
 
-| ELet x e1 v1 h' b h'' e2 v2 h'''
-  of eval_expr ls h e1 v1 h'
-  &  alloc h' 1 = (b, h'')
-  &  eval_expr (setm ls x b) h'' e2 v2 h'''
-  :  eval_expr ls h (Let x e1 e2) v2 (free h''' b)
+    | Store e e' =>
+      if eval_expr ls e is VPtr p then
+        if updm h p (eval_expr ls e') is Some h' then Done ls h'
+        else Error
+      else Error
 
-| EAss l p h' e v h'' h'''
-  of eval_loc ls h l p h'
-  &  eval_expr ls h e v h''
-  &  updm h'' p v = Some h'''
-  :  eval_expr ls h (Ass l e) v h'''
+    | Alloc x e =>
+      if eval_expr ls e is VNum (Posz n) then
+        let: (i, h') := alloc h n in
+        Done (setm ls x (VPtr (i, 0))) h'
+      else Error
 
-| EIf e1 b h' e2 e3 v h''
-  of eval_expr ls h e1 (VBool b) h'
-  &  eval_expr ls h (if b then e2 else e3) v h''
-  :  eval_expr ls h (If e1 e2 e3) v h''
+    | Free e =>
+      if eval_expr ls e is VPtr p then
+        if fpick [pred p' | p'.1 == p.1] (domm h) then
+          Done ls (free h p.1)
+        else Error
+      else Error
 
-| EWhileEnd e1 h' e2
-  of eval_expr ls h e1 (VBool false) h'
-  :  eval_expr ls h (While e1 e2) (VNum 0) h'
+    | Skip => Done ls h
 
-| EWhileLoop e1 h' e2 v h'' v' h'''
-  of eval_expr ls h   e1 (VBool true) h'
-  &  eval_expr ls h'  e2 v h''
-  &  eval_expr ls h'' (While e1 e2) v' h'''
-  :  eval_expr ls h   (While e1 e2) v' h'''.
+    | Seq c1 c2 =>
+      let r1 := eval_com ls h c1 k' in
+      if r1 is Done ls' h' then
+        eval_com ls' h' c2 k'
+      else r1
+
+    | If e ct ce =>
+      if eval_expr ls e is VBool b then
+        eval_com ls h (if b then ct else ce) k'
+      else Error
+
+    | While e c =>
+      if eval_expr ls e is VBool b then
+        eval_com ls h (if b then While e c else Skip) k'
+      else Error
+    end
+  else NotYet.
+
+Definition refine_result r1 r2 :=
+  (r1 == NotYet) || (r1 == r2).
+
+Lemma eval_com_leq ls h c k k' :
+  (k <= k')%N ->
+  refine_result (eval_com ls h c k) (eval_com ls h c k').
+Proof.
+move=> Pk; elim: k' k Pk ls h c => [|k' IH] [|k] // /IH {IH} IH ls h.
+rewrite /refine_result.
+case=> [x e|x e|e e'|x e|e| |c1 c2|e ct ce|e c] /=; try by rewrite eqxx ?orbT.
+- case/orP: (IH ls h c1) => [/eqP -> //|/eqP ->].
+  case: (eval_com _ _ c1 _) => [ls' h'| |] //=.
+  by eauto.
+- by case: (eval_expr ls e) => [b| | |] //=; eauto.
+by case: (eval_expr ls e) => [b| | |] //=; eauto.
+Qed.
 
 End Def.
